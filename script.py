4cabe3915fa4610fc24595d9ec609ec135dc92c9
Andrea <26469651+AndreaJasper@users.noreply.github.com>
2024-01-23 16:56:11 -0700

    Update script.py
    added a few prints to better legibility in editor

--- script.py
+++ script.py
@@ -13,8 +13,8 @@ while total_cost <= len(actual_insurance_costs):
 print("Total cost: " + str(total_cost))
 
 average_cost = total_cost/len(actual_insurance_costs)
-
 print("The average insurance cost is: " + str(average_cost))
+print(" ----- ")
 
 for i in range(len(names)):
   name = names[i]
@@ -25,9 +25,11 @@ for i in range(len(names)):
 
   if insurance_costs > average_cost:
     print("The insurance cost for " + name + " is above average by " + str(cost_difference))
+    print(" ----- ")
   elif insurance_costs < average_cost:
     print("The insurance cost for " + name + " is below average by " + str(cost_difference))
+    print(" ----- ")
   else:
     print("The insurance cost for " + name + " is equal to average by " + str(cost_difference))
-
-print(updated_estimated_costs)
+    print(" ----- ")
+print("Estimated insurance costs are 10% higher than originally thought. The updated costs are as follows: " + str(updated_estimated_costs))
